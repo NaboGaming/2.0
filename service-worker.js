100c1817e4b649df9fe3f012da58c5b502d0a937
Nabo-Gaming <156772627+NaboGaming@users.noreply.github.com>
2024-05-28 15:31:45 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'nabo-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/css/styles.css', // Add all your assets here
+    '/nabo.jpg',
+    '/logo2.gif',
+    '/pubgmain.jpg',
+    '/assets/js/bootstrap.bundle.min' // Your main JavaScript file
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
