4be243c3322e0dc013edb4109096cd0dcbf73e91
Nabo-Gaming <156772627+NaboGaming@users.noreply.github.com>
2024-05-28 17:33:40 +0300

    Update pwa-services.js

--- assets/js/pwa-services.js
+++ assets/js/pwa-services.js
@@ -1,10 +1,13 @@
 if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/service-worker.js')
-                    .then(registration => {
-                        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                    }, err => {
-                        console.log('ServiceWorker registration failed: ', err);
-                    });
-            });
-        }
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(error => {
+            console.error('ServiceWorker registration failed: ', error);
+        });
+    });
+} else {
+    console.log('Service Workers are not supported in this browser.');
+}
